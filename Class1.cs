929f5d44741cba2a2b526fce4b3905a4ce816515
djavo666aca <52077531+djavo666aca@users.noreply.github.com>
2023-12-08 15:56:14 +0100

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,32 @@
+﻿using Eksterni_NameSpace_i_klase;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace Eksterni_NameSpace_i_klase
+{
+    public class Class1
+    {
+        public static int x = 1;
+        public static string NekiString = "Neki string iz eksterne klase";
+        public static char NekiChar = 'Z';
+    }
+}
+namespace DrugiNS 
+{
+public class Class2 
+    {
+    public static void PrintMetod() 
+        {
+        Class1 objekat = new Class1();
+            Console.WriteLine("#####################");
+            Console.WriteLine(Class1.x);
+            Console.WriteLine(Class1.NekiChar);
+            Console.WriteLine(Class1.NekiString);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using DrugiNS;
+
+namespace Eksterni_NameSpace_i_klase
+
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Main Program");
+            Console.WriteLine(Class1.x);
+            Console.WriteLine(Class1.NekiString);
+            Console.WriteLine(Class1.NekiChar);
+
+            Class2.PrintMetod();
+
+            
+        }
+    }
+}
